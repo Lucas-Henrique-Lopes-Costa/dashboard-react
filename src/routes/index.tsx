7a54bc8b671cd5f2c707f5633a9e71eb87cccb4f
Lucas Henrique Lopes Costa <lucass20586@gmail.com>
2023-08-04 00:27:44 -0300

    feat: theme

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,10 +1,10 @@
 import { Routes, Route, Navigate } from "react-router-dom";
+import { Button } from "@mui/material";
 
 export const AppRoutes = () => {
   return (
     <Routes>
-      <Route path="/pagina-inicial" element={<p>Página Inicial</p>} /> 
-      {/* se não encontra vai para a rota inicial */}
+      <Route path="/pagina-inicial" element={<p><Button variant="contained" color="primary">Teste</Button></p>} /> {/* se não encontra vai para a rota inicial */}
       <Route path="*" element={<Navigate to="pagina-inicial"/>} />
     </Routes>
   );
---
+++ src/shared/themes/Light.ts
@@ -0,0 +1,24 @@
+import { createTheme } from '@mui/material'
+import { cyan, yellow } from '@mui/material/colors'
+
+export const LightTheme = createTheme({
+  palette: {
+    primary: {
+      // Usado em botões, links e etc
+      light: yellow[500],
+      main: yellow[700],
+      dark: yellow[800],
+      contrastText: '#fff'
+    },
+    secondary: {
+      light: cyan[500],
+      main: cyan[700],
+      dark: cyan[800],
+      contrastText: '#fff'
+    },
+    background: {
+      default: '#f7f6f3',
+      paper: '#fff'
+    }
+  }
+})

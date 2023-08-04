a1ce33a42752c15f6d4a283fecc5d146957f11e4
Lucas Henrique Lopes Costa <lucass20586@gmail.com>
2023-08-04 07:43:12 -0300

    feat: Tema escuro e Troca de Temas

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
 import { BrowserRouter } from "react-router-dom";
-import { ThemeProvider } from "@mui/material";
-import { LightTheme } from "./shared/themes";
+
 import { AppRoutes } from "./routes";
+import { AppThemeProvider } from "./shared/contexts/ThemeContext";
 
 export const App = () => {
   return (
-    <ThemeProvider theme={LightTheme}>
+    <AppThemeProvider>
       <BrowserRouter>
         <AppRoutes />
       </BrowserRouter>
-    </ThemeProvider>
+    </AppThemeProvider>
   );
 }
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,10 +1,13 @@
 import { Routes, Route, Navigate } from "react-router-dom";
 import { Button } from "@mui/material";
+import { useAppThemeContext } from "../shared/contexts";
 
 export const AppRoutes = () => {
+  const { toggleTheme } = useAppThemeContext();
+  
   return (
     <Routes>
-      <Route path="/pagina-inicial" element={<p><Button variant="contained" color="primary">Teste</Button></p>} /> {/* se não encontra vai para a rota inicial */}
+      <Route path="/pagina-inicial" element={<p><Button variant="contained" color="primary" onClick={toggleTheme}>Trocar de Tema</Button></p>} /> {/* se não encontra vai para a rota inicial */}
       <Route path="*" element={<Navigate to="pagina-inicial"/>} />
     </Routes>
   );
